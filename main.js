ac00b9e57219b84a8f31a5af55e021acdced5714
Hossain Razavi <hossain.razavi@gmail.com>
2022-05-29 16:31:39 +0430

    JS first step

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-let counter_element = document.body.querySelector(".box1 .counter-number");
-let counter_value = counter_element.textContent;
+function counterIncrement() {
+let counterElement = document.body.querySelector(".box1 .counter-number");
+let counterValue = counterElement.textContent;
+counterElement.textContent = Number(counterValue) + 2;
+}
+setInterval(counterIncrement,1401);
+
+function walletsIncrement() {
+    let counterElement = document.body.querySelector(".box2 .counter-number");
+    let counterValue = counterElement.textContent;
+    counterElement.textContent = Number(counterValue) + 2;
+    }
+setInterval (walletsIncrement,5000);
+
+function validatorIncrement() {
+    let counterElement = document.body.querySelector(".box3 .counter-number");
+    let counterValue = counterElement.textContent;
+    counterElement.textContent = Number(counterValue) + 2;
+    }
+setInterval (validatorIncrement,60000);
+
+//Topics Window
+let topicSide = document.body.querySelector(".window-topic:nth-child(2)");
+function nextPage(){
+}
+
+topicSide.addEventListener('click',nextPage);
