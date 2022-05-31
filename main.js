9e9a66a4d7382faf20bbda0bb08aa506d67f1cb1
Hossain Razavi <hossain.razavi@gmail.com>
2022-05-31 13:15:45 +0430

    js halfway

--- main.js
+++ main.js
@@ -20,8 +20,22 @@ function validatorIncrement() {
 setInterval (validatorIncrement,60000);
 
 //Topics Window
-let topicSide = document.body.querySelector(".window-topic:nth-child(2)");
-function nextPage(){
-}
+let topicSide = document.body.querySelectorAll(".window-topic");
+let topicWindow=document.body.querySelectorAll('window-main');
+topicSide.forEach(function topicIterator(topic,index) {
+    topic.addEventListener("click", function onClick(e) {
+        topicSide.forEach(function disableTopic(element){
+            element.classList.remove("selected-topic");
+        });
+        let elementSelected=e.target;
+        elementSelected.classList.add("selected-topic");
+        topicWindow.forEach(function hideContent(element){
+            element.classList.remove('window-shown');
+        });
+        let shownContent = topicSide[index];
+        shownContent.classList.add('window-shown');
+    });
 
-topicSide.addEventListener('click',nextPage);
+});
+topicSide[0].classList.add("selected-topic");
+// topicWindow[0].classList.add('window-shown');
